4faa7fc7bdc7eddc716c893675241ad56ff0eb77
14353350 <928413156@qq.com>
2017-05-18 20:52:47 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/yc/kuou/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.yc.kuou.util;
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by lenovo on 2017/5/18.
+ */
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
