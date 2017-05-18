336b670d219e3ce04c5d6c30c81bd87f0173ad6f
14353350 <928413156@qq.com>
2017-05-18 16:37:42 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/yc/kuou/db/Province.java
@@ -0,0 +1,37 @@
+package com.yc.kuou.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by lenovo on 2017/5/18.
+ */
+public class Province extends DataSupport{
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+}
